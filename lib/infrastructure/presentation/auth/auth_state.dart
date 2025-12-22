11885cd031c88ab91767e8d4878479471fbfce2d
VitorFranciscoDev <vitorfndr@gmail.com>
2025-12-22 14:29:14 -0300

    feature(): auth state started

--- lib/infrastructure/presentation/auth/auth_state.dart
+++ lib/infrastructure/presentation/auth/auth_state.dart
@@ -1,7 +1,14 @@
 import 'package:daily_track/domain/entities/user.dart';
+import 'package:daily_track/domain/usecases/user_usecases.dart';
 import 'package:flutter/material.dart';
 
 class AuthProvider with ChangeNotifier {
+  final UserUseCases _useCases;
+
+  AuthProvider({
+    required UserUseCases useCases,
+  }) : _useCases = useCases;
+
   User? _user;
   User? get user => _user;
 
@@ -11,4 +18,19 @@ class AuthProvider with ChangeNotifier {
   bool _isLoading = false;
   bool get isLoading => _isLoading;
 
+  Future<User?> login(String email, String password) async {
+    return await _useCases.login(email, password);
+  }
+
+  Future<void> addUser(User user) async {
+    return await _useCases.addUser(user);
+  }
+
+  Future<void> deleteUser(String uuid) async {
+    return await _useCases.deleteUser(uuid);
+  }
+
+  Future<void> updateUser(User user) async {
+    return await _useCases.updateUser(user);
+  }
 }
